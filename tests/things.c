0adbe199d330ba3572ff19e8fea880e3058b62b4
Chris <chris.clark@coffeeb4code.com>
2022-02-26 11:15:05 -0600

    mocking complete

--- tests/things.c
+++ tests/things.c
@@ -1,16 +1,16 @@
 #define NOBUILD_IMPLEMENTATION
+#define WITH_MOCKING
 #include "../include/things.h"
 #include "../nobuild.h"
 #include <stdio.h>
 
-void test_add_4() {
-  ASSERT(add_4(3) == 7);
-  ASSERT(add_9() == 12);
-}
+void test_add_4() { ASSERT(add_4(3) == 7); }
+
+DECLARE_MOCK(int, add_2);
 
 int main() {
-  MOCK0(int, add_9, 12);
   DESCRIBE("things");
+  MOCK(add_2, 2);
   SHOULDF("add 4 to input", test_add_4);
   RETURN();
 }
