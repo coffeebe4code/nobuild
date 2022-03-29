7d57bf56423aeff8605002d96e100ba0fb0d09a0
Chris Clark <78776922+coffeebe4code@users.noreply.github.com>
2022-03-29 14:07:49 -0500

    better mocking of structs (#25)
    * better mocking of structs and va args added for multi argument functions

--- include/stuff.h
+++ include/stuff.h
@@ -1,2 +1,8 @@
 #pragma once
-extern int add_2(const int val);
+int add_2(const int val);
+
+typedef struct {
+  int i;
+} example_t;
+
+example_t add_2_t(const int val);
--- src/stuff/add.c
+++ src/stuff/add.c
@@ -1,2 +1,6 @@
 #include "../../include/stuff.h"
 int add_2(const int val) { return val + 2; }
+example_t add_2_t(const int val) {
+  int added = val + 2;
+  return (example_t){.i = added};
+}
