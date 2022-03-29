60ffdf4f9db69015ca4e8bf1faa983a49f3548bf
Chris <chris.clark@coffeeb4code.com>
2022-03-29 15:54:39 -0500

    added another test

--- src/things/lib.c
+++ src/things/lib.c
@@ -1,6 +1,8 @@
 #include "../../include/stuff.h"
 #include "stdio.h"
 
+int do_something_again() { return do_something() + do_something(); }
+
 int add_4(int num) {
   int one = add_2(0);
   int two = add_2(0);
